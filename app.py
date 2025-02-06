24592fc7faa44f49910faedfa0433674fa4c32b0
mishra-git <35693012+mishra-git@users.noreply.github.com>
2025-02-05 21:52:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"message": "Welcome to Visual Poet!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)), reload=True)
+
